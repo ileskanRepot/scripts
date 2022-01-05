a6d2bbfc8e65d88d6d70be90bb658d08bcf3f822
ileskanRepot <ileska@tutanota.com>
2022-01-06 00:06:48 +0200

    Download currently iin mpv playing song

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 # randomKeyboardLayout.sh
 Set main keys (a..z) to random positions in keyboard
+
+# downloadCurrentMpvSong.sh
+Download currently in mpv playing song
---
+++ downloadCurrentMpvSong.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SONG=$(echo '{ "command": ["get_property", "path"] }' | socat - /tmp/mpvsocket | jq -r ".data" | sed 's/mkv/webm/g')
+
+if [ "$(echo $SONG | head -c4)" == "http" ];
+then
+	yt-dlp -x --audio-format mp3 "$SONG"
+else
+	SONGNAME=$(echo $SONG | awk -F/ '{print $NF}' | sed 's/.webm//g')
+	SONGREPLACED=$(echo "$SONG" | sed 's/mkv/webm/g')
+	ffmpeg -i "$SONGREPLACED" -f mp3 -ab 320000 -vn ~/music/"$SONGNAME.mp3"
+fi
