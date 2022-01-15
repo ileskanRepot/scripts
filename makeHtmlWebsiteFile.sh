400f5d6c1b1ebbbec8002ce62c997aa8972f5545
ileskanRepot <ileska@tutanota.com>
2022-01-15 16:13:20 +0200

    Make a simple html website folder

---
+++ makeHtmlWebsiteFile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$1" = "" ];
+then
+	echo Usage:
+	echo makeHtmlWebsiteFile.sh \<website name\>
+	exit 1
+fi
+mkdir "$1"
+echo -e "<!DOCTYPE html>\n<!--\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿\n⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿\n⣿⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿\n⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿\n⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿\n⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿\n⣿⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿\n⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿\n⣿⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿\n⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿\n⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿\n-->\n<html lang=\"fi\">\n<head>\n	<meta charset=\"utf-8\">\n	<title>I L E S K A · I N D U S T R I E S</title>\n	<meta name=\"description\" content=\"\">\n	<meta name=\"author\" content=\"@ileska\">\n	<link rel=\"stylesheet\" href=\"outfit.css\">\n	<link rel=\"icon\" type=\"image/png\" href=\"SivuIcon.png\">\n\n</head>\n\n<body>\n\t<h1>$1</h1>\n</body>\n</html>" >> "$1"/index.html
+echo -e "body, html {\n\tposition: absolute;\n\twidth: 100%;\n\theight: 100%;\n\tmargin: 0;\n\tbackground-color: black;\n}\n\nh1 {\n\ttext-align: center;\n\tcolor: white;\n}" >> "$1"/outfit.css
