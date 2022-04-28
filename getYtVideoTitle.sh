63ccb3308d74cac3dd551085db64a441c3875bc8
Ileska <ileska@tutanota.com>
2022-04-28 16:22:23 +0300

    Add odysee support

--- getYtVideoTitle.sh
+++ getYtVideoTitle.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 [ -z "$1" ] && echo -e "Usage:\ngetYtVideoTitle.sh [Video URL]" && exit
-JSON=$(curl -s "https://www.youtube.com/oembed?url=$1&format=json" | jq .)
-TITLE=$(echo $JSON | jq -r .title)
-AUTH_NAME=$(echo $JSON | jq -r .author_name)
+
+URL=$1
+if [ -z $(echo "$URL" | grep "odysee") ];
+then
+	JSON=$(curl -s "https://www.youtube.com/oembed?url=$URL&format=json" | jq .)
+	TITLE=$(echo $JSON | jq -r .title)
+	AUTH_NAME=$(echo $JSON | jq -r .author_name)
+else
+	JSON=$(curl "$URL" | sed -n "/\<script type=\"application\/ld/,/\/script/p" | grep -v script) 
+	TITLE=$(echo $JSON | jq -r ."name")
+	AUTH_NAME="ODYSEE"
+fi
+
 notify-send "Play video $TITLE" "From $AUTH_NAME"
 echo -e "$TITLE\n$AUTH_NAME"
