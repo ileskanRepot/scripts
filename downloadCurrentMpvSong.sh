7db1f79c1bfce21a91a1d288ca4d86f77181b183
Ileska <ileska@tutanota.com>
2022-04-28 16:23:27 +0300

    Change format to opus and set start/done notifications

--- downloadCurrentMpvSong.sh
+++ downloadCurrentMpvSong.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 SONG=$(echo '{ "command": ["get_property", "path"] }' | socat - /tmp/mpvsocket | jq -r ".data" | sed 's/mkv/webm/g')
+echo $SONG
 NAME=$(echo $SONG | grep -o '[^/]*$')
 notify-send "Start" "Downloading $NAME"
-echo '{ "command": ["get_property", "path"] }' | socat - /tmp/mpvsocket
 
 if [ "$(echo $SONG | head -c4)" == "http" ];
 then
-	yt-dlp -x --audio-format mp3 "$SONG"
+	yt-dlp -x --audio-format mp3 "$SONG" && notify-send "Finish" "Downloaded $NAME" || notify-send "Error" "Error downloading $NAME"
 else
 	SONGNAME=$(echo $SONG | awk -F/ '{print $NF}' | sed 's/.webm//g')
-	SONGREPLACED=$(echo "$SONG" | sed 's/mkv/webm/g')
-	ffmpeg -i "$SONGREPLACED" -f mp3 -ab 320000 -vn ~/music/"$SONGNAME.mp3"
+	echo "'$SONGNAME'"
+	SONGREPLACED=$(echo "$SONG" | sed 's/mkv/webm/g' | sed 's/ /\\ /g' | tr -d '\\')
+	echo "$SONGREPLACED" | tr -d '\\'
+	ffmpeg -i "$SONGREPLACED" -f opus -ab 320000 -vn ~/music/"$SONGNAME.opus" && notify-send "Finish" "Downloaded $NAME" || notify-send "Error" "Error downloading $NAME"
 fi
 
-notify-send "Finish" "Downloaded $NAME"
+
